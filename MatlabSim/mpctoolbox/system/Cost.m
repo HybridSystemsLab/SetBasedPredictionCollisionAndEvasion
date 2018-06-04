5ad2ddfed27e34c156ed983ee4cd9057bafa991a
Jeremy Crowley <jcrowley@ucsc.edu>
2018-06-04 10:36:04 -0700

    Main files for each minimization problem. Organization

--- MatlabSim/mpctoolbox/system/Cost.m
+++ MatlabSim/mpctoolbox/system/Cost.m
@@ -1,12 +1,12 @@
 % function c = Cost(x0, u, ts, target)
 
 function c = Cost(x0, u, ts, target)
-
     
-    x = SingleIntegrator(x0,u,ts)
+    % predict system state
+    x = SingleIntegrator(x0,u,ts);
    
+    % calculate cost of prediction
     [m,n] = size(x);
-    
     c = 0;
     for i = 1:n
         c = c + norm(x(:,i)-target);
